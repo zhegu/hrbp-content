097885cd7b83fd3271530a93a96dbecfb817b576
zhegu707 <affe@live.cn>
2017-05-19 17:40:59 +0800

    312
    12312

--- js/api/flowAnaly/api.flow.analy.callback.js
+++ js/api/flowAnaly/api.flow.analy.callback.js
@@ -5,6 +5,7 @@ function cbPositionStabilityIndex(json) {
     if (checkCallBackJsonIsNotNull(json) && checkJsonDateIsNotNull(json.data)) {
         $.each(json.data, function (i, item) {
             $('#position_stability_index').html(item.index);
+            scaleMap2('post_change_twig', 'h-ltwig', 'h-rtwig', item.index, getRProportion(item.index, 0));
         })
         empInPostIndex();
     } else {
@@ -19,6 +20,7 @@ function cbEmpInPostIndex(json) {
     if (checkCallBackJsonIsNotNull(json) && checkJsonDateIsNotNull(json.data)) {
         $.each(json.data, function (i, item) {
             $('#emp_in_post_index').html(item.index);
+            scaleMap2('post_unusual_twig', 'h-ltwig', 'h-rtwig', item.index, getRProportion(item.index, 0));
         })
         turnoverIntentionIndex();
     } else {
@@ -33,6 +35,7 @@ function cbTurnoverIntentionIndex(json) {
     if (checkCallBackJsonIsNotNull(json) && checkJsonDateIsNotNull(json.data)) {
         $.each(json.data, function (i, item) {
             $('#turnover_intention_index').html(item.index);
+            scaleMap2('leave_twig', 'h-ltwig', 'h-rtwig', item.index, getRProportion(item.index, 0));
         })
         remainInCity();
     } else {
@@ -47,6 +50,7 @@ function cbRemainInCity(json) {
     if (checkCallBackJsonIsNotNull(json) && checkJsonDateIsNotNull(json.data)) {
         $.each(json.data, function (i, item) {
             $('#remain_in_city').html(item.index);
+            scaleMap2('leave_direction_twig', 'h-ltwig', 'h-rtwig', item.index, getRProportion(item.index, 0));
         })
     } else {
         alert(json.message);
