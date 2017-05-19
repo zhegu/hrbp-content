e9646dcedb89f15c03f5e1672d6e471e449e6065
zhegu707 <affe@live.cn>
2017-05-19 17:08:48 +0800

    12
    34

--- js/api/norm/api.norm.callback.js
+++ js/api/norm/api.norm.callback.js
@@ -1,10 +1,10 @@
 function cbSpecificationIndex(json) {
-
     alert('cbSpecificationIndex');
     alert(json);
     if (checkCallBackJsonIsNotNull(json) && checkJsonDateIsNotNull(json.data)) {
         $.each(json.data, function (i, item) {
             $('#specification_index').html(item.index);
+            scaleMap2('norm_index_twig', 'h-ltwig', 'h-rtwig', item.index, getRProportion(item.index, 0));
         })
         zengGangIndex();
     } else {
@@ -19,6 +19,7 @@ function cbZengGangIndex(json) {
     if (checkCallBackJsonIsNotNull(json) && checkJsonDateIsNotNull(json.data)) {
         $.each(json.data, function (i, item) {
             $('#zenggang_index').html(item.index);
+            scaleMap2('norm_increase_twig', 'h-ltwig', 'h-rtwig', item.index, getRProportion(item.index, 0));
         })
         demobilizedIndex();
     } else {
@@ -33,6 +34,7 @@ function cbDemobilizedIndex(json) {
     if (checkCallBackJsonIsNotNull(json) && checkJsonDateIsNotNull(json.data)) {
         $.each(json.data, function (i, item) {
             $('#demobilized_index').html(item.index);
+            scaleMap2('norm_leave_twig', 'h-ltwig', 'h-rtwig', item.index, getRProportion(item.index, 0));
         })
     } else {
         alert(json.message);
